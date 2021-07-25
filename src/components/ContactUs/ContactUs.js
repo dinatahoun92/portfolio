4678df82dda63a3e43aa5588bfb06483d9aa4eb4
Dina Tahoun <dinatahoun992@gmail.com>
2021-07-25 21:44:45 +0200

    contaxt img

--- src/components/ContactUs/ContactUs.js
+++ src/components/ContactUs/ContactUs.js
@@ -1,7 +1,7 @@
 import React, { useRef } from "react";
 import emailjs from "emailjs-com";
 import { useAlert } from "react-alert";
-
+import contact from "./contact.svg";
 import styles from "./ContactUs.module.css";
 
 export default function ContactUs() {
@@ -32,21 +32,31 @@ export default function ContactUs() {
 
   return (
     <div className={styles.main}>
-      <h4 className={styles.subtitle}>Send Me A Message!</h4>
-      <h1 className={styles.title}>
-        Contact <span>Me!</span>
-      </h1>
-      <p className={styles.text}>
-        You can send me an email to <span>dinatahoun992@gmail.com</span> or you
-        can use this form to send me a message.
-      </p>
-      <form className={styles.form} onSubmit={sendEmail} ref={formEl}>
-        <input type="hidden" name="contact_number" />
-        <input type="text" name="user_name" required placeholder="Name" />
-        <input type="email" name="user_email" required placeholder="Email" />
-        <textarea name="message" placeholder="Message" />
-        <input type="submit" value="Send" className="button" />
-      </form>
+      <div className={styles.container}>
+        <div className={styles.left}>
+          <h4 className={styles.subtitle}>Send Me A Message!</h4>
+          <h1 className={styles.title}>
+            Contact <span>Me!</span>
+          </h1>
+          <p className={styles.text}>
+            You can send me an email to <span>dinatahoun992@gmail.com</span> or
+            you can use this form to send me a message.
+          </p>
+          <form className={styles.form} onSubmit={sendEmail} ref={formEl}>
+            <input type="hidden" name="contact_number" />
+            <input type="text" name="user_name" required placeholder="Name" />
+            <input
+              type="email"
+              name="user_email"
+              required
+              placeholder="Email"
+            />
+            <textarea name="message" placeholder="Message" />
+            <input type="submit" value="Send" className="button" />
+          </form>
+        </div>
+        <img src={contact} className={styles.contactImg} />
+      </div>
     </div>
   );
 }
